c7d64111128b2a9bea9f4f817c673fc0aed41e54
nikita_228 <poletaevn@protonmail.com>
2023-06-15 19:54:26 +0300

    Added logic to filter buttons

--- src/app/events/event-details/session-list/session-list.component.ts
+++ src/app/events/event-details/session-list/session-list.component.ts
@@ -1,12 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ISession } from '../../shared';
 
 @Component({
-  selector:'session-list',
+  selector: 'session-list',
   templateUrl: './session-list.component.html',
 })
-export class SessionListComponent {
-  @Input() sessions!: ISession[]
+export class SessionListComponent implements OnChanges {
+  @Input() sessions!: ISession[];
+  @Input() filterBy: string = 'all';
+  visibleSessions: ISession[] = []
 
-  constructor() { }
+
+  filterSessions(filter: string) {
+    filter === 'all' ? this.visibleSessions = this.sessions.slice(0)
+      : this.visibleSessions = this.sessions.filter(s => s.level.toLocaleLowerCase() === filter);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!this.sessions) return;
+    this.filterSessions(this.filterBy)
+  }
 }
